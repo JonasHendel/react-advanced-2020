abe0bbb9ae297fe42ef3c95b57211512ef1d364a
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-03-01 21:57:45 +0100

    finished useState

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Fragment } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+	const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person)=> person.id !== id)
+    setPeople(newPeople)
+  }
+
+	return (
+		<Fragment>
+			{people.map((person) => {
+				const { id, name } = person;
+				return (
+					<div key={id} className='item'>
+						<h4>{name}</h4>
+            <button className='btn' onClick={()=> removeItem(id)}>Remove</button>
+					</div>
+				);
+			})}
+			<button className='btn' onClick={() => setPeople([])}>Clear Items</button>
+		</Fragment>
+	);
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+	const [value, setValue] = useState(0);
+
+	const reset = () => {
+		setValue(0);
+	};
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
+
+	return (
+		<React.Fragment>
+			<section style={{ margin: '4rem 9' }}>
+				<h2>Regular Counter</h2>
+				<h1>{value}</h1>
+				<button className='btn' onClick={() => setValue(value + 1)}>
+					Increase
+				</button>
+				<button className='btn' onClick={() => setValue(value - 1)}>
+					Decrease
+				</button>
+				<button className='btn' onClick={reset}>
+					Reset
+				</button>
+			</section>
+      <section style={{ margin: '4rem 9' }}>
+				<h2>Complex Counter</h2>
+				<h1>{value}</h1>
+				<button className='btn' onClick={complexIncrease}>
+					Increase
+				</button>
+			</section>
+		</React.Fragment>
+	);
 };
 
 export default UseStateCounter;
