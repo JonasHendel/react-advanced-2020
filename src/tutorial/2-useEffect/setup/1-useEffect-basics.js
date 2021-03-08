71a2df61830c5d968050e7217a1aeb54c880a5cd
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-03-08 19:06:46 +0100

    worked on useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,21 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+  const [value, setValue] = useState(0)
+
+  useEffect(() => {
+    if(value >= 1){
+      document.title = `New Messages(${value})`
+    }
+  }) // [] => only runs on first render [value] => runs whenever value is changed
+
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => {setValue(value + 1)}}>Increase value</button>
+    </React.Fragment>
+  )
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,24 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+	const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(()=>{
+      window.addEventListener('resize', checkSize)
+      console.log(size)
+      return ()=>{
+        window.removeEventListener('resize', checkSize)
+        console.log('cleanup')
+      }//this return is called after the second render, size is Displayed once, but when it is rerendered, the return is called
+  })
+	return(
+    <React.Fragment>
+    <h1>Window</h1>      
+    <h2>{size} PX</h2>
+    </React.Fragment>
+  ) 
 };
 
 export default UseEffectCleanup;
