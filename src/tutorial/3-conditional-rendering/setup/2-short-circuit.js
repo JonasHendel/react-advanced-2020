63e855a5977b9c2e68d57fbd8fefbae585d7e9e6
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-03-14 21:35:04 +0100

    finished ShowHide

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,17 +3,24 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+	const [text, setText] = useState('');
+	const [isError, setIsError] = useState(false);
 
-  return(
-    <React.Fragment>
-      <h1>{text || 'jonas hendel'}</h1>
-      {text && <h1>Hello world</h1>}
-      {!text && <h1>Bye world</h1>}
-    </React.Fragment>
-  ) 
+	return (
+		<React.Fragment>
+			<h1>{text || 'jonas hendel'}</h1>
+			{text && <h1>Hello world</h1>}
+			<button className='btn' onClick={() => setIsError(!isError)}>
+				toggle error
+			</button>
+			{isError && <h1>Error</h1>}
+      {isError ? (
+        <p>There is an error</p>
+      ):(
+        <p>There is no error</p>
+      )}
+		</React.Fragment>
+	);
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false)
+
+  return (
+    <React.Fragment>
+      <button className='btn' onClick={()=> setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item/>}
+    </React.Fragment>
+  )
+  
+  
 };
 
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.addEventListener('resize', checkSize)
+    }
+  },[])
+
+  return (
+    <React.Fragment>
+      <h1>size: {size}</h1>
+    </React.Fragment>
+  )
+}
 export default ShowHide;
